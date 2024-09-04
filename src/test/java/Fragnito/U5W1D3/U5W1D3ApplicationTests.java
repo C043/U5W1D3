736fd4c6f8a6fd4a03a1f7d31ce9f4e8361dbb8a
C043 <mariofragnitoph@gmail.com>
2024-09-04 15:25:12 +0200

    Update U5W1D3ApplicationTests.java

--- src/test/java/Fragnito/U5W1D3/U5W1D3ApplicationTests.java
+++ src/test/java/Fragnito/U5W1D3/U5W1D3ApplicationTests.java
@@ -1,13 +1,36 @@
 package Fragnito.U5W1D3;
 
+import Fragnito.U5W1D3.entities.*;
+import Fragnito.U5W1D3.enums.StatoTavolo;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class U5W1D3ApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static Stream<Arguments> orderGen(){
+		return Stream.of(
+				Arguments.of(new Ordine(new Tavolo(1, 2, StatoTavolo.LIBERO), 1, List.of(new Pizza("1", 100, 10, new ArrayList<>()), new Bevanda("4", 20, 5, 0.50)), 2, 2.00), 19.00),
+				Arguments.of(new Ordine(new Tavolo(1, 2, StatoTavolo.LIBERO), 1, List.of(new Pizza("2", 100, 20, new ArrayList<>()), new Topping("test", 500, 0)), 2, 2.00), 24.00),
+				Arguments.of(new Ordine(new Tavolo(1, 2, StatoTavolo.LIBERO), 1, List.of(new Pizza("3", 100, 50, new ArrayList<>()), new Bevanda("test", 20, 10, 0.80), new Topping("test", 300, 5)), 4, 2.00), 73.00)
+		);
 	}
 
+	@ParameterizedTest
+	@MethodSource("orderGen")
+	void checkOrderTotal(Ordine ordine, double totalExpected){
+		assertEquals(totalExpected, ordine.getTotaleOrdine());
+	}
 }
